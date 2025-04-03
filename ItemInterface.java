4aadd77ee747579089c576654228b25102c580c7
calebelee05 <calebelee05@gmail.com>
2025-04-03 11:26:23 -0400

    Implemented interfaces

---
+++ ItemInterface.java
@@ -0,0 +1,18 @@
+/**
+ * A ItemInterface interface
+ *
+ * Purdue University -- CS18000 -- Spring 2025 -- Team Project -- Phase 1
+ *
+ * @author Caleb Lee
+ * @version April 3, 2025
+ */
+public interface ItemInterface {
+    String getName();
+    double getPrice();
+    String getDescription();
+    int getItemID();
+    void setItemID(int itemID);
+    void setPrice(int price);
+    void setDescription(String description);
+    void setName(String itemName);
+}
---
+++ MessageInterface.java
@@ -0,0 +1,20 @@
+import java.time.ZonedDateTime;
+
+/**
+ * A MessageInterface interface
+ *
+ * Purdue University -- CS18000 -- Spring 2025 -- Team Project -- Phase 1
+ *
+ * @author Caleb Lee
+ * @version April 3, 2025
+ */
+public interface MessageInterface {
+    String getSenderID();
+    void setSenderID(String senderID);
+    String getRecipientID();
+    void setRecipientID(String recipientID);
+    String getContents();
+    void setContents(String contents);
+    ZonedDateTime getTimestamp();
+    void setTimeStamp(ZonedDateTime timestamp);
+}
