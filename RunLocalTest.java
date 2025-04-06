70d7e7d81a8c29fd3568cb43100ffdf3fe1a93ff
calebelee05 <calebelee05@gmail.com>
2025-04-06 17:17:11 -0400

    Modified RunLocalTest

--- RunLocalTest.java
+++ RunLocalTest.java
@@ -12,13 +12,4 @@ public class RunLocalTest {
 		
       System.out.println(result.wasSuccessful());
    }
-
-   public static class TestFunctions {
-    // Set the input
-    String user1Input = "User1\nabcd1234\n1000.0\nItem1\n10.0\nTest Item1\nHello\nUser2\n";
-        
-    String user2Input = "User2\nqwer0000\n3500.0\nItem2\n15.0\nTest Item2\nHi\nUser1\n";
-
-    
-    }
 }  	
